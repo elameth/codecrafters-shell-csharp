7b6cd15a74d08b5bbe7d6a4e7fd10be4e4330b0c
elameth <creepyelameth@gmail.com>
2025-12-26 21:07:31 +0300

    PATH check added try 1

--- src/main.cs
+++ src/main.cs
@@ -1,5 +1,41 @@
 class Program
 {
+    static bool IsExecutable(string path)
+    {
+        var extension = Path.GetExtension(path);
+        if (string.IsNullOrEmpty(extension))
+            return false;
+
+        var pathExtensions = Environment.GetEnvironmentVariable("PATHEXT");
+        if (string.IsNullOrEmpty(pathExtensions))
+            return false;
+
+        return pathExtensions
+            .Split(';', StringSplitOptions.RemoveEmptyEntries)
+            .Any(e => e.Equals(extension, StringComparison.OrdinalIgnoreCase));
+    }
+
+    static string? FindExecutableInPath(string fileName)
+    {
+        var paths = Environment.GetEnvironmentVariable("PATH")?
+            .Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries);
+        if (paths == null)
+            return null;
+        foreach (var path in paths)
+        {
+            var fullPath = Path.Combine(path, fileName);
+            if (File.Exists(fullPath) && IsExecutable(fullPath))
+                return fullPath;
+                
+        }
+        return null;
+    }
+
+    
+    
+    
+    
+    
     static void Main()
     {
         while (true)
@@ -20,6 +56,11 @@ class Program
                             Console.WriteLine($"{input[1]} is a shell builtin");
                             break;
                         default:
+                            var fullPath = FindExecutableInPath(input[1]);
+                            if (fullPath != null)
+                            {
+                                Console.WriteLine($"{input[1]} is {fullPath}");
+                            }
                             Console.WriteLine($"{input[1]}: not found");
                             break;
                             
@@ -36,8 +77,6 @@ class Program
                     break;
             }
         }
-        
-        
     }
 }
 
